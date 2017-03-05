685d1a4bbdcc8d1f6b534af36702d35b04358c64
伪红学家 <pengwang@cmu.edu>
2017-03-05 16:51:52 -0500

    Separate urls from webapps

---
+++ bigbox/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import logout_then_login
+from django.core.urlresolvers import reverse_lazy
+
+urlpatterns = [
+    url(r'^$', views.login, name='root'),
+    url(r'^login/?$', views.login, name='login'),
+    url(r'^logout/?$', logout_then_login, {'login_url': reverse_lazy('login')}, name='logout'),
+    url(r'^register/?$', views.register, name='register'),
+    url(r'^home/?$', views.home, name='home'),
+    url(r'^confirm/(?P<username>\w+)/(?P<token>[\w-]+)/?$', views.confirm, name='confirm'),
+    url(r'^clouds/add/(?P<cloud>\w+)/?$', views.add_storage_account, name='cloud-add'),
+]
--- webapps/urls.py
+++ webapps/urls.py
@@ -1,16 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from bigbox import views
-from django.contrib.auth.views import logout_then_login
-from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
-    url(r'^$', views.login, name='root'),
     url(r'^admin/', admin.site.urls, name='admin'),
-    url(r'^login/?$', views.login, name='login'),
-    url(r'^logout/?$', logout_then_login, {'login_url': reverse_lazy('login')}, name='logout'),
-    url(r'^register/?$', views.register, name='register'),
-    url(r'^home/?$', views.home, name='home'),
-    url(r'^confirm/(?P<username>\w+)/(?P<token>[\w-]+)/?$', views.confirm, name='confirm'),
-    url(r'^clouds/add/(?P<cloud>\w+)/?$', views.add_storage_account, name='cloud-add'),
+    url(r'^', include('bigbox.urls'))
 ]
