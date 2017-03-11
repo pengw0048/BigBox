ad337ec4a5d91c485d99c8491581d32e814aa7f7
伪红学家 <pengwang@cmu.edu>
2017-03-10 23:45:06 -0500

    Use HTTP REST for gdrive's oauth flow and force to get refresh token

--- bigbox/gdrive_interface.py
+++ bigbox/gdrive_interface.py
@@ -1,34 +1,35 @@
 from django.http import *
 from django.conf import settings
 from django.contrib import messages
-from oauth2client import client
 from .models import *
 from dateutil import parser
 import json
 from datetime import datetime, timedelta
 import requests
+from urllib.parse import urlencode
 
 
 def add_storage_account(request, next_url, cloud):
-    flow = client.OAuth2WebServerFlow(settings.GDRIVE_APP_KEY, client_secret=settings.GDRIVE_APP_SECRET,
-                                      scope=settings.GDRIVE_SCOPE, redirect_uri=settings.GDRIVE_REDIRECT_URL)
-    flow.params['access_type'] = 'offline'
     if 'error' in request.GET:
         error_message = 'An error occurred: ' + request.GET['error']
         messages.error(request, error_message)
         return HttpResponseRedirect(next_url)
     elif 'code' in request.GET:
         try:
-            credentials = flow.step2_exchange(request.GET['code'])
-            uid = credentials.id_token['sub']
-            r = requests.get("https://www.googleapis.com/drive/v3/about", params={'fields': 'user'},
-                             headers={'Authorization': 'Bearer ' + credentials.access_token})
+            r = requests.post("https://www.googleapis.com/oauth2/v4/token",
+                              {'code': request.GET['code'], 'client_id': settings.GDRIVE_APP_KEY,
+                               'client_secret': settings.GDRIVE_APP_SECRET,
+                               'redirect_uri': settings.GDRIVE_REDIRECT_URL, 'grant_type': 'authorization_code'})
+            credentials = r.json()
+            expire_at = (datetime.utcnow() + timedelta(0, credentials['expires_in'])).isoformat()
+            access_token = credentials['access_token']
+            refresh_token = credentials['refresh_token']
+            r = requests.get("https://www.googleapis.com/oauth2/v3/userinfo",
+                             headers={'Authorization': 'Bearer ' + access_token})
             about = r.json()
-            full_name = about['user']['displayName']
-            email = about['user']['emailAddress']
-            access_token = credentials.access_token
-            refresh_token = credentials.refresh_token
-            expire_at = credentials.token_expiry.isoformat()
+            uid = about['sub']
+            full_name = about['name']
+            email = about['email']
         except Exception as e:
             messages.error(request, 'An error occurred: ' + str(e))
         else:
@@ -42,8 +43,11 @@ def add_storage_account(request, next_url, cloud):
                 messages.success(request, 'A new Google Drive space is now linked to your account')
         return HttpResponseRedirect(next_url)
     else:
-        auth_uri = flow.step1_get_authorize_url()
-        return HttpResponseRedirect(auth_uri)
+        url = 'https://accounts.google.com/o/oauth2/v2/auth'
+        param = urlencode({'client_id': settings.GDRIVE_APP_KEY, 'redirect_uri': settings.GDRIVE_REDIRECT_URL,
+                           'response_type': 'code', 'scope': settings.GDRIVE_SCOPE, 'access_type': 'offline',
+                           'prompt': 'consent select_account'})
+        return HttpResponseRedirect(url + '?' + param)
 
 
 def get_client(acc: StorageAccount) -> str:
