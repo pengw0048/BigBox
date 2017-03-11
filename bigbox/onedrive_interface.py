31f7e59fcee984d401a4b7d9431eccf80d1fb6d8
伪红学家 <pengwang@cmu.edu>
2017-03-11 15:09:05 -0500

    Switch onedrive to use HTTP REST

--- bigbox/onedrive_interface.py
+++ bigbox/onedrive_interface.py
@@ -2,19 +2,14 @@ from django.http import *
 from django.conf import settings
 from django.contrib import messages
 from .models import *
-import onedrivesdk
-import asyncio
 import requests
-from onedrivesdk.session import Session
 import json
-from time import time
 from dateutil import parser
+from datetime import datetime, timedelta
+from urllib.parse import urlencode
 
 
 def add_storage_account(request, next_url, cloud):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    client = onedrivesdk.get_default_client(settings.ONEDRIVE_APP_KEY, settings.ONEDRIVE_SCOPE.split(","))
     if 'error' in request.GET:
         error_message = 'An error occurred'
         if 'error_description' in request.GET:
@@ -23,29 +18,36 @@ def add_storage_account(request, next_url, cloud):
         return HttpResponseRedirect(next_url)
     elif 'code' in request.GET:
         try:
-            client.auth_provider.authenticate(request.GET['code'], settings.ONEDRIVE_REDIRECT_URL,
-                                              settings.ONEDRIVE_APP_SECRET)
-            access_token = client.auth_provider._session.access_token
+            r = requests.post("https://login.microsoftonline.com/common/oauth2/v2.0/token",
+                              {'code': request.GET['code'], 'client_id': settings.ONEDRIVE_APP_KEY,
+                               'client_secret': settings.ONEDRIVE_APP_SECRET,
+                               'redirect_uri': settings.ONEDRIVE_REDIRECT_URL, 'grant_type': 'authorization_code'})
+            credentials = r.json()
+            expire_at = (datetime.utcnow() + timedelta(0, credentials['expires_in'])).isoformat()
+            access_token = credentials['access_token']
+            refresh_token = credentials['refresh_token']
             info = get_user_info(access_token)
-            id = info['id']
+            uid = info['id']
             full_name = info['displayName']
             short_name = info['givenName']
             email = info['userPrincipalName']
         except Exception as e:
             messages.error(request, 'An error occurred: ' + str(e))
         else:
-            if StorageAccount.objects.all().filter(identifier=id).exists():
+            if StorageAccount.objects.all().filter(identifier=uid).exists():
                 messages.warning(request, 'This OneDrive space is already linked')
             else:
-                sa = StorageAccount(user=request.user, cloud=cloud, identifier=id, status=1,
-                                    credentials=MySession.save_session(client.auth_provider._session),
+                sa = StorageAccount(user=request.user, cloud=cloud, identifier=uid, status=1,
+                                    credentials=json.dumps({'a': access_token, 'r': refresh_token, 'e': expire_at}),
                                     user_full_name=full_name, user_short_name=short_name, email=email)
                 sa.save()
                 messages.success(request, 'A new OneDrive space is now linked to your account')
         return HttpResponseRedirect(next_url)
     else:
-        auth_url = client.auth_provider.get_auth_url(settings.ONEDRIVE_REDIRECT_URL)
-        return HttpResponseRedirect(auth_url)
+        url = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+        param = urlencode({'client_id': settings.ONEDRIVE_APP_KEY, 'redirect_uri': settings.ONEDRIVE_REDIRECT_URL,
+                           'response_type': 'code', 'scope': settings.ONEDRIVE_SCOPE})
+        return HttpResponseRedirect(url + '?' + param)
 
 
 def get_user_info(access_token):
@@ -53,29 +55,35 @@ def get_user_info(access_token):
     return r.json()
 
 
-def get_client(acc: StorageAccount):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    http_provider = onedrivesdk.HttpProvider()
-    auth_provider = onedrivesdk.AuthProvider(http_provider, settings.ONEDRIVE_APP_KEY,
-                                             settings.ONEDRIVE_SCOPE.split(","), session_type=MySession)
-    auth_provider.load_session(cred=acc.credentials)
-    auth_provider.refresh_token()
-    client = onedrivesdk.OneDriveClient(settings.ONEDRIVE_BASE_URL, auth_provider, http_provider)
-    acc.credentials = MySession.save_session(client.auth_provider._session)
-    acc.save()
-    return client
+def get_client(acc: StorageAccount) -> str:
+    cred = json.loads(acc.credentials)
+    expire_at = parser.parse(cred['e'])
+    delta = (expire_at - datetime.utcnow()).total_seconds()
+    if delta < 60:
+        r = requests.post("https://login.microsoftonline.com/common/oauth2/v2.0/token",
+                          {'client_id': settings.ONEDRIVE_APP_KEY, 'client_secret': settings.ONEDRIVE_APP_SECRET,
+                           'refresh_token': cred['r'], 'grant_type': 'refresh_token',
+                           'redirect_uri': settings.ONEDRIVE_REDIRECT_URL})
+        if r.status_code == 200:
+            j = r.json()
+            cred['a'] = j['access_token']
+            if 'refresh_token' in j:
+                cred['r'] = j['refresh_token']
+            cred['e'] = (datetime.utcnow() + timedelta(0, j['expires_in'])).isoformat()
+            acc.credentials = json.dumps(cred)
+            acc.save()
+    return cred['a']
 
 
-def get_space(od: onedrivesdk.OneDriveClient) -> dict:
+def get_space(od: str) -> dict:
     r = requests.get(settings.ONEDRIVE_BASE_URL + "me/drive",
-                     headers={'Authorization': 'bearer ' + od.auth_provider._session.access_token}).json()
+                     headers={'Authorization': 'bearer ' + od}).json()
     used = r['quota']['used']
     total = r['quota']['total']
     return {'used': used, 'total': total}
 
 
-def get_file_list(od: onedrivesdk.OneDriveClient, path: str) -> list:
+def get_file_list(od: str, path: str) -> list:
     ret = []
     if path == '/':
         path = "drive/root/children"
@@ -83,7 +91,7 @@ def get_file_list(od: onedrivesdk.OneDriveClient, path: str) -> list:
         path = "drive/root:" + path + ":/children"
     try:
         fs = requests.get(settings.ONEDRIVE_BASE_URL + path,
-                          headers={'Authorization': 'bearer ' + od.auth_provider._session.access_token}).json()
+                          headers={'Authorization': 'bearer ' + od}).json()
         for f in fs['value']:
             try:
                 if 'file' in f:
@@ -98,28 +106,12 @@ def get_file_list(od: onedrivesdk.OneDriveClient, path: str) -> list:
     return ret
 
 
-def get_down_link(od: onedrivesdk.OneDriveClient, fid: str) -> str:
+def get_down_link(od: str, fid: str) -> str:
     r = requests.get(settings.ONEDRIVE_BASE_URL + "drive/items/" + fid + '/content',
-                     headers={'Authorization': 'bearer ' + od.auth_provider._session.access_token})
+                     headers={'Authorization': 'bearer ' + od})
     if r.status_code < 300 or r.status_code >= 400:
         r.raise_for_status()
     if r.url:
         return r.url
     else:
         raise Exception('File not found')
-
-
-class MySession(Session):
-    def save_session(self, **save_session_kwargs):
-        data = {'token_type': self.token_type, 'scope': ' '.join(self.scope), 'access_token': self.access_token,
-                'client_id': self.client_id, 'auth_server_url': self.auth_server_url,
-                'redirect_uri': self.redirect_uri, 'refresh_token': self.refresh_token,
-                'client_secret': self.client_secret, 'expires_at': self._expires_at}
-        return json.dumps(data)
-
-    def load_session(**load_session_kwargs):
-        data = json.loads(load_session_kwargs['cred'])
-        session = Session(data['token_type'], data['expires_at'] - time(), data['scope'], data['access_token'],
-                          data['client_id'], data['auth_server_url'], data['redirect_uri'], data['refresh_token'],
-                          data['client_secret'])
-        return session
