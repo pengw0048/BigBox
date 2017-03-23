a3aa622959cef16037b1288b135f4e24b2981e01
伪红学家 <pengwang@cmu.edu>
2017-03-23 17:31:34 -0400

    Use HTTP Rest to authorize to Dropbox

--- bigbox/dropbox_interface.py
+++ bigbox/dropbox_interface.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlencode
+
+import requests
 from django.conf import settings
 from django.contrib import messages
 from django.http import *
@@ -8,35 +11,41 @@ from pytz import timezone
 from .models import *
 
 
-def get_dropbox_auth_flow(web_app_session):
-    return DropboxOAuth2Flow(settings.DROPBOX_APP_KEY, settings.DROPBOX_APP_SECRET, settings.DROPBOX_REDIRECT_URL,
-                             web_app_session, "dropbox-auth-csrf-token")
-
-
 def add_storage_account(request, next_url, cloud):
-    if 'state' in request.GET:
-        try:
-            oauth_result = get_dropbox_auth_flow(request.session).finish(request.GET)
-            db = Dropbox(oauth_result.access_token)
-            ai = db.users_get_current_account()
-        except Exception as e:
-            error_message = 'An error occurred'
-            if 'error' in request.GET and 'error_description' in request.GET:
-                error_message += ' (%s): %s' % (request.GET['error'], request.GET['error_description'])
-            messages.error(request, error_message + "\r\n" + str(e))
+    if 'error' in request.GET:
+        error_message = 'An error occurred (%s)' % request.GET['error']
+        if 'error_description' in request.GET:
+            error_message += ': %s' % (request.GET['error_description'])
+        messages.error(request, error_message + "\r\n")
+        return HttpResponseRedirect(next_url)
+    elif 'code' in request.GET:
+        r = requests.post('https://api.dropboxapi.com/oauth2/token',
+                          data={'code': request.GET['code'], 'grant_type': 'authorization_code',
+                                'client_id': settings.DROPBOX_APP_KEY, 'client_secret': settings.DROPBOX_APP_SECRET,
+                                'redirect_uri': settings.DROPBOX_REDIRECT_URL})
+        j = r.json()
+        account_id = j['account_id']
+        access_token = j['access_token']
+        r = requests.post('https://api.dropboxapi.com/2/users/get_account', json={'account_id': account_id},
+                          headers={'Authorization': 'Bearer ' + access_token})
+        j = r.json()
+        full_name = j['name']['display_name']
+        short_name = j['name']['abbreviated_name']
+        email = j['email']
+        if StorageAccount.objects.all().filter(identifier=account_id).exists():
+            messages.warning(request, 'This Dropbox space is already linked')
         else:
-            if StorageAccount.objects.all().filter(identifier=oauth_result.account_id).exists():
-                messages.warning(request, 'This Dropbox space is already linked')
-            else:
-                sa = StorageAccount(user=request.user, cloud=cloud, identifier=oauth_result.account_id, status=1,
-                                    credentials=oauth_result.access_token, user_full_name=ai.name.display_name,
-                                    user_short_name=ai.name.abbreviated_name, email=ai.email)
-                sa.save()
-                messages.success(request, 'A new Dropbox space is now linked to your account')
+            sa = StorageAccount(user=request.user, cloud=cloud, identifier=account_id, status=1,
+                                credentials=access_token, user_full_name=full_name,
+                                user_short_name=short_name, email=email)
+            sa.save()
+            messages.success(request, 'A new Dropbox space is now linked to your account')
         return HttpResponseRedirect(next_url)
     else:
-        url = get_dropbox_auth_flow(request.session).start()
-        return HttpResponseRedirect(url)
+        url = 'https://www.dropbox.com/oauth2/authorize'
+        params = urlencode({'response_type': 'code', 'client_id': settings.DROPBOX_APP_KEY,
+                            'redirect_uri': settings.DROPBOX_REDIRECT_URL, 'require_role': 'personal'})
+        return HttpResponseRedirect(url + '?' + params)
 
 
 def get_client(acc: StorageAccount) -> Dropbox:
