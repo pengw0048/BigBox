c0a503a69242222eec07919abd83879bc458b908
polydier <JudyZhu16@gmail.com>
2017-04-02 21:05:50 -0400

    add recaptcha widget to login page, add recaptcha validation to setting

--- webapps/settings.py
+++ webapps/settings.py
@@ -144,3 +144,5 @@ ONEDRIVE_APP_SECRET = config.get('Onedrive', 'Secret')
 ONEDRIVE_REDIRECT_URL = config.get('Onedrive', 'Redirect')
 ONEDRIVE_BASE_URL = config.get('Onedrive', 'Base')
 ONEDRIVE_SCOPE = config.get('Onedrive', 'Scope')
+
+GOOGLE_RECAPTCHA_SECRET_KEY = '6LenRxsUAAAAAPrnhyc2j_Rdj53X6YnbfiH9Ae5u'
