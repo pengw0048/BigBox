0ef700d30d23381d703f9894532dd44befd6f6d8
伪红学家 <pengwang@cmu.edu>
2017-04-11 16:53:42 -0400

    Add icons of this website

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.views import logout_then_login
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import RedirectView
 
 from . import views
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^$', views.login, name='root'),
@@ -20,4 +23,5 @@ urlpatterns = [
     url(r'^clouds/add/(?P<cloud>\w+)/?$', views.add_storage_account, name='cloud-add'),
     url(r'^clouds/rename/?$', views.rename_storage_account, name='cloud-rename'),
     url(r'^clouds/color/?$', views.color_storage_account, name='cloud-color'),
+    url(r'^favicon\.ico$', favicon_view),
 ]
